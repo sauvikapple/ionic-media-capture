019b92c514b3a1d0f79743ce7d75c8f10bb716bc
sauvik dolui <sauvikdolui@gmail.com>
2021-11-23 00:56:50 +0530

    Initial commit

---
+++ android/src/main/java/com/sauvikdolui/plugins/mediacapture/MediaCapture.java
@@ -0,0 +1,11 @@
+package com.sauvikdolui.plugins.mediacapture;
+
+import android.util.Log;
+
+public class MediaCapture {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
